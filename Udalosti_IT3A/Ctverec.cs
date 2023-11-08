4bf3a1dd2438a3ee8df053a18b1af79eda1bb161
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-11-08 12:30:59 +0100

    Add project files.

---
+++ Udalosti_IT3A/Ctverec.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace Udalosti_IT3A
+{
+  public class Ctverec
+  {
+    private List<Brush> barvy = new List<Brush>()
+    {
+      Brushes.Red,
+      Brushes.Green,
+      Brushes.Blue,
+      Brushes.Magenta,
+      Brushes.Yellow,
+      Brushes.Orange,
+      Brushes.Orchid
+    };
+    private int vybranaBarva = 0;
+    private DispatcherTimer timer;
+    private Random random = new Random();
+
+    public double Velikost { get; private set; }
+    public Brush Barva { get => barvy[vybranaBarva]; }
+
+    public Action? BarvaZmenena;
+
+    public Ctverec(double velikost)
+    {
+      Velikost = velikost;
+      timer = new DispatcherTimer();
+      timer.Interval = new TimeSpan(0, 0, random.Next(1, 6));
+      timer.Tick += Timer_Tick;
+      timer.Start();
+    }
+
+    private void Timer_Tick(object? sender, EventArgs e)
+    {
+      timer.Interval = new TimeSpan(0, 0, random.Next(1, 6));
+      vybranaBarva = random.Next(0, barvy.Count);
+      BarvaZmenena?.Invoke();
+    }
+  }
+}
